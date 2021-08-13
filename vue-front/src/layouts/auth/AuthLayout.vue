b205d74a22bf6d9612640d5f58015d33af84ef8e
everysoon <minsoon1269@naver.com>
2021-08-13 15:23:25 +0900

    시작하기 페이지 만드는중 : start_home 제작 사용자에 의해 컴포넌트 추가/삭제 어떻게 할지 생각하기

---
+++ vue-front/src/layouts/auth/AuthLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app >
+    <v-app-bar-nav-icon  @click="clickDrawer" />
+    <soon-title class="d-flex mt-15 mb-5 justify-center"/>
+    <drawer :value="drawer" @clickDrawer="changeDrawer"/>
+    <router-view/>
+  </v-app>
+</template>
+
+<script>
+import SoonTitle from '@/components/button/SoonTitle'
+import Drawer    from "@/components/Drawer";
+
+export default {
+  name      : "AuthLayout",
+  components: {Drawer, SoonTitle},
+  data:()=>({
+    drawer:null
+  })
+  ,
+  methods:{
+    clickDrawer(){
+      this.drawer = !this.drawer;
+    },
+    changeDrawer(val) {
+      this.drawer = !val;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
